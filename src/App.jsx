286428c8ea5ab639c3e6db1c54f7f67ce2de20ed
anagabrieleloor <anagabrieleloor@outlook.com>
2023-08-07 15:54:32 -0400

    ugh

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState} from 'react'
 import './App.css'
 import ContactList from './components/ContactList';
+import {useEffect} from 'react'
+
 
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] =useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
     </>
   );
 }
-
-
